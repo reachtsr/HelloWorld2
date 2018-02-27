ee966b60ad51a43647f4279db1b11350c41325f9
sridhar <sri@wonderwidgets>
2018-02-26 21:40:02 -0500

    code updated

--- Hello.java
+++ Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("Jenkins Test Job. Date-02/26/2018");
+		System.out.println("Jenkins Test Job Push to GitHub. Date-02/26/2018");
 		
 	}
 
